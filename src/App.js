6227cbee7b9c32055a585052486172d01e9954b0
Estela Diaz <estela.diaz@segment.com>
2018-09-24 12:47:46 -0700

    learning about refs

--- src/App.js
+++ src/App.js
@@ -4,28 +4,32 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      currentEvent: '---'
+      a: '',
+      b: ''
     }
-    this.update = this.update.bind(this)
   }
 
   update(e) {
     this.setState({
-      currentEvent: e.type
+      a: this.refs.a.value,
+      b: this.refs.b.value
     })
   }
 
   render() {
     return (
       <div>
-        <textarea
-          onKeyPress={this.update}
-          onCopy={this.update}
-          onCut={this.update}
-          onPaste={this.update}
-          cols="30"
-          rows="10"/>
-          <h1>{this.state.currentEvent}</h1>
+        <input
+          ref="a"
+          type="text"
+          onChange={this.update.bind(this)}
+        /> {this.state.a}
+        <hr/>
+        <input
+          ref="b"
+          type="text"
+          onChange={this.update.bind(this)}
+        /> {this.state.b}
       </div>
     )
   }
